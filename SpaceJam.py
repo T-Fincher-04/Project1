49949a6e1ecc0269623fe157511b5ae9a55b0d11
T-Fincher-04 <tfincher1@student.cscc.edu>
2026-02-24 14:52:34 -0500

    Added required code and two additional files. Also changed player from Phaser to Dumbledore.

--- SpaceJam.py
+++ SpaceJam.py
@@ -1,11 +1,13 @@
 from direct.showbase.ShowBase import ShowBase
-
+import DefensePaths as DefensePaths
+import SpaceJamClasses
 
 class MyApp(ShowBase):
 
     def __init__(self):
         ShowBase.__init__(self)
         self.SetupScene()
+        self.InitializeDefenses()
     
         
         
@@ -13,38 +15,78 @@ class MyApp(ShowBase):
             self.Universe = self.loader.loadModel("./Assets/Universe/Universe.glb")
             self.Universe.reparentTo(self.render)
             self.Universe.setScale(15000)
+            
             self.Planet1 = self.loader.loadModel("./Assets/Planets/Planet1.glb")
             self.Planet1.reparentTo(self.render)
             self.Planet1.setPos(150, 5000, 67)
             self.Planet1.setScale(100)
+
             self.Planet2 = self.loader.loadModel("./Assets/Planets/Planet2.glb")
             self.Planet2.reparentTo(self.render)
             self.Planet2.setPos(500, 5000, 67)
             self.Planet2.setScale(100)
+            
             self.Planet3 = self.loader.loadModel("./Assets/Planets/Planet3.glb")
             self.Planet3.reparentTo(self.render)
             self.Planet3.setPos(1000, 5000, 67)
             self.Planet3.setScale(100)
+
             self.Planet4 = self.loader.loadModel("./Assets/Planets/Planet4.glb")
             self.Planet4.reparentTo(self.render)
             self.Planet4.setPos(1500, 5000, 67)
             self.Planet4.setScale(100)
+
             self.Planet5 = self.loader.loadModel("./Assets/Planets/Planet5.glb")
             self.Planet5.reparentTo(self.render)
             self.Planet5.setPos(2000, 5000, 67)
             self.Planet5.setScale(100)
+
             self.Planet6 = self.loader.loadModel("./Assets/Planets/Planet6.glb")
             self.Planet6.reparentTo(self.render)
             self.Planet6.setPos(2500, 5000, 67)
             self.Planet6.setScale(100)
-            self.Spaceship = self.loader.loadModel("./Assets/Spaceships/phaser.egg")
+
+            self.Spaceship = self.loader.loadModel("./Assets/Spaceships/Dumbledore.egg")
             self.Spaceship.reparentTo(self.render)
             self.Spaceship.setPos(-1000, 4000, 67)
             self.Spaceship.setScale(50)
+            
             self.Space_Station = self.loader.loadModel("./Assets/Space Station/spaceStation.egg")
             self.Space_Station.reparentTo(self.render)
             self.Space_Station.setPos(2000, 3000, 67)
             self.Space_Station.setScale(100)
+
+            self.Universe = SpaceJamClasses.Universe(self.loader, "./Assets/Universe/Universe.glb", self.render, "Universe", "./Assets/Universe/Universe.png", (0, 0, 0), 15000)
+            self.Planet1 = SpaceJamClasses.Planet(self.loader, "./Assets/Planets/Planet1.glb", self.render, "Planet1", "./Assets/Planets/Planet1.png", (150, 5000, 67), 100)
+            self.Planet2 = SpaceJamClasses.Planet(self.loader, "./Assets/Planets/Planet2.glb", self.render, "Planet2", "./Assets/Planets/Planet2.png", (500, 5000, 67), 100)
+            self.Planet3 = SpaceJamClasses.Planet(self.loader, "./Assets/Planets/Planet3.glb", self.render, "Planet3", "./Assets/Planets/Planet3.png", (1000, 5000, 67), 100)
+            self.Planet4 = SpaceJamClasses.Planet(self.loader, "./Assets/Planets/Planet4.glb", self.render, "Planet4", "./Assets/Planets/Planet4.png", (1500, 5000, 67), 100)
+            self.Planet5 = SpaceJamClasses.Planet(self.loader, "./Assets/Planets/Planet5.glb", self.render, "Planet5", "./Assets/Planets/Planet5.png", (2000, 5000, 67), 100)
+            self.Planet6 = SpaceJamClasses.Planet(self.loader, "./Assets/Planets/Planet6.glb", self.render, "Planet6", "./Assets/Planets/Planet6.png", (2500, 5000, 67), 100)
+            self.Spaceship = SpaceJamClasses.Spaceship(self.loader, "./Assets/Spaceships/Dumbledore.egg", self.render, "Spaceship", "./Assets/Spaceships/spacejet_C.png", (-1000, 4000, 67), 50)
+            self.Space_Station = SpaceJamClasses.Space_Station(self.loader, "./Assets/Space Station/spaceStation.egg", self.render, "Space_Station", "./Assets/Space Station/SpaceStation1_Dif2.png", (2000, 3000, 67), 100)
+            
+    def DrawBaseballSeams(self, centralObject, droneName, step, numSeams, radius = 1):
+            unitVec = DefensePaths.BaseballSeams(step, numSeams, B = 0.4)
+            unitVec.normalize()
+            position = unitVec * radius * 250 + centralObject.modelNode.getPos()
+            SpaceJamClasses.Drone(self.loader, "./Assets/DroneDefender/DroneDefender.obj", self.render, droneName, "./Assets/DroneDefender/octotoad1_auv.png", position, 5)
+
+    def DrawCloudDefense(self, centralObject, droneName):
+        unitVec = DefensePaths.Cloud()
+        unitVec.normalize()
+        position = unitVec * 500 + centralObject.modelNode.getPos()
+        SpaceJamClasses.Drone(self.loader, "./Assets/DroneDefender/DroneDefender.obj", self.render, droneName, "./Assets/DroneDefender/octotoad1_auv.png", position, 10)
+
+   
+    def InitializeDefenses(self):
+        fullcycle = 60
+        for j in range(fullcycle):
+            SpaceJamClasses.Drone.droneCount += 1
+            nickname = "Drone" + str(SpaceJamClasses.Drone.droneCount)
+            self.DrawCloudDefense(self.Planet1, nickname)
+            self.DrawBaseballSeams(self.Space_Station, nickname, j, fullcycle, 2)
+   
             
 
 
